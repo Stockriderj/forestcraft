fe2f2274bff5ea2bf62c80edf14ef6e88fd197b3
Stockriderj <78283807+Stockriderj@users.noreply.github.com>
2021-05-25 11:24:36 -0500

    Updated Changelog

---
+++ changelogScript.js
@@ -0,0 +1,43 @@
+const changelog = `
+v0.2.5 - Added health bar!
+<br>
+v0.2.3 - Apples can now rot
+<br>
+v0.2 - Major bug fixes
+<br>
+v0.1.5.15 - Multiple apples now drop
+<br>
+v0.1.5.12 - Added item drops upon death
+<br>
+v0.1.5.8 - Major bug fixes
+<br>
+v0.1.5.5 - Inital scroller transition and fixes, though ironically I just made it worse.
+<br>
+v0.1.5.18 - Made leaves slow you down and fixes
+<br>
+v0.1.5.16 - You can now climb trees!
+<br>
+v0.1.5.11 - Gathering wood can now hurt you
+<br>
+v0.1.5.10 - Added wood (useless)
+<br>
+v0.1.5.9.2 - Added grass and blood particles
+<br>
+v0.1.5.7 - Apples now fall from trees
+<br>
+v0.1.5.5 - Created space between trees (more pleasant world generation)
+<br>
+v0.1.3.4 - Added health and fall damage; apples now heal you
+<br>
+v0.1.1.14 - Tree layering
+<br>
+v0.1.1.10 - Initial sound update (trees)
+<br>
+v0.1.1.8 - Added and refurbished apples
+<br>
+v0.1.1 - Added clunky save/load engine
+<br>
+v0.1 - Initial share
+`;
+
+document.getElementById("changelog").innerHTML = changelog;
