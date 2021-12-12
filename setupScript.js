d15b643615984be10e80f662cc24ffc204f4be21
Stockriderj <78283807+Stockriderj@users.noreply.github.com>
2021-12-11 18:12:27 -0600

    Update setupScript.js

--- setupScript.js
+++ setupScript.js
@@ -1,7 +1,7 @@
 const code = `
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: forestgreen;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../index.html"><img src="../images/logo.png" alt="ForestCraft" class="img-fluid rounded-pill" style="width: 40px;"></a>
+    <a class="navbar-brand" href="index.html"><img src="../images/logo.png" alt="ForestCraft" class="img-fluid rounded-pill" style="width: 40px;"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -11,10 +11,10 @@ const code = `
           <a class="nav-link" aria-current="page" href="../index.html">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../play.html">Play</a>
+          <a class="nav-link" href="play.html">Play</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../gallery.html">Gallery</a>
+          <a class="nav-link" href="gallery.html">Gallery</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
